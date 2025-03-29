dbfa2085f3a959dd7c21b6dba1dc5123b72a647d
henryantwi <henryantwi191@gmail.com>
2025-03-29 13:29:07 +0000

    Refactor templates and views for improved structure and functionality
    - Updated docker-compose.yaml to comment out unused environment variables for PostgreSQL.
    - Added new views (dashboard, invest, about) in a_profile/views.py and updated the profile view.
    - Refactored URL routing in core/urls.py to include new views from a_profile.
    - Converted static HTML templates (about.html, confirmation.html, dashboard.html, invest.html, login.html, password.html, profile.html, register.html, support.html, updates.html) to extend a new base template and utilize Django's static file handling.
    - Created a new base.html template to standardize layout and include dynamic title and CSS/JS links.
    - Added a new a_profile/urls.py file to manage routing for profile-related views.

--- src/a_profile/views.py
+++ src/a_profile/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def profile(request):
     return render(request, "profile.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+def invest(request):
+    return render(request, "invest.html")
+
+def about(request):
+    return render(request, "about.html")
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from a_profile.views import profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile/', profile, name="profile"),
+    path('', include('a_profile.urls')),
 ]
